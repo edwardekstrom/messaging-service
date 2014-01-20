c6f4b7fd7f046e1c671e2b746fc80b57079dd351
Edward Ekstrom <edwardekstrom@edwardekstrom-MacBookAir.(none)>
2014-01-20 10:02:33 -0700

    added serverprotocol.cc

---
+++ serverprotocol.cc
@@ -0,0 +1,18 @@
+/*
+ * serverprotocol.cc
+ *
+ *  Created on: Jan 20, 2014
+ *      Author: edwardekstrom
+ */
+
+#include "ServerProtocol.h"
+
+ServerProtocol::ServerProtocol() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ServerProtocol::~ServerProtocol() {
+	// TODO Auto-generated destructor stub
+}
+
